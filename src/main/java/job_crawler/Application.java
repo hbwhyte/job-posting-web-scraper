0df8d72a885fe2355e6725b2bde3589572946fbd
hbwhyte <heather.b.whyte@gmail.com>
2018-04-17 21:26:08 +0800

    setup for remote deployment

--- src/main/java/job_crawler/Application.java
+++ src/main/java/job_crawler/Application.java
@@ -3,17 +3,17 @@ package job_crawler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-// import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
-    //public class Application extends SpringBootServletInitializer {
+//public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -26,15 +26,15 @@ public class Application {
     @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
-        pool.setCorePoolSize(20);
-        pool.setMaxPoolSize(30);
+        pool.setCorePoolSize(10);
+        pool.setMaxPoolSize(15);
         pool.setWaitForTasksToCompleteOnShutdown(true);
         return pool;
     }
 
     // For connecting to AWS server
-//    @Override
-//    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-//        return application.sources(Application.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
 }
